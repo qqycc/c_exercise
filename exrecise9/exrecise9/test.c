99168eca34fbf3548319cf57a855adf6d77fe014
qqycc <963933327@qq.com>
2018-10-26 10:12:02 +0800

    结构体

--- exrecise9/exrecise9/test.c
+++ exrecise9/exrecise9/test.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//自定指一种类型，该类型的变量用来表示一个学生
+//抽象
+typedef struct Student{
+	char name[1024];
+	int score;
+}Student;
+
 int main(){
-	int arr[5] = { 9,7,5,3,1};
-	int* p = &arr[0];
-	int* p1 = &arr[1];
-	if (p < p1){
-		printf("p<p1\n");
-	}
-	else{
-		printf("p>=p1\n");
-	}
+	struct Student student = {"张三",90}; 
+	Student* p = &student;
+	printf("姓名：%s\n分数：%d\n",student.name,p->score);
 	system("pause");
 	return 0;
-	}
-
+}
 
 //int main(){
-//
+//	typedef int* int_ptr;
+//	int n = 100;
+//	int_ptr p = &n;
+//	int_ptr* pp = &p;
 //	system("pause");
 //	return 0;
 //}
+
+//int main(){
+//	int arr[5] = { 9,7,5,3,1};
+//	int* p = &arr[0];
+//	int* p1 = &arr[1];
+//	if (p < p1){
+//		printf("p<p1\n");
+//	}
+//	else{
+//		printf("p>=p1\n");
+//	}
+//	system("pause");
+//	return 0;
+//	}
+
+
 
 //int main(){
 //	int n = 0x11223344;  //内存存的位44 33 22 11
