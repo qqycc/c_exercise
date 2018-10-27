e903fbbfe10fb1932a75115723b2a1b70d175ee3
qqycc <963933327@qq.com>
2018-10-27 10:23:05 +0800

    练习12完善

--- exercise12/exercise12/test.c
+++ exercise12/exercise12/test.c
@@ -1,14 +1,123 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+//student a am i 
+//i ma a tneduts 
+//i am a student 
 
-//一组数据中只有一个数字出现了一次。其他所有数字都是成对出现的。 
-//请找出这个数字。（使用位运算）
+
+int Strlen(char* str){
+	//str指向的是一个空字符串
+	if (*str == '\0'){
+		return 0;
+	}
+	//str指向的不是一个空字符串，当前字符是字符串中的一个元素
+	return 1 + strlen(1 + str);
+}
+
+//数组被函数调用，隐式退化为指针
+void Flip(char *str, int sz)
+{ 
+	char *str1 = str;
+	char *left = str;
+	char *right = str + sz - 1;
+	while (left < right) { 
+		char temp = *left;
+		*left = *right;
+		*right = temp;
+		left++;
+		right--;
+	}     
+	while (*str1)
+	{
+		left = str1;
+		while ((*str1 != '\0') && (*str1 != ' '))
+		{
+			str1++;
+		}
+		right = str1 - 1;               
+		while (left < right) {
+			char temp = *left;
+			*left = *right;
+			*right = temp;
+			left++;
+			right--;
+		}
+		if (*str1 == ' ')            
+		{
+			str1++;
+		}
+	}
+}
+
 int main(){
-
+	char arr[] = "student a am i";
+	int length = Strlen(arr);
+	//翻转
+	Flip(arr,length);
+	printf("%s\n", arr);
 	system("pause");
 	return 0;
 }
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int arr[11] = { 11,10,1,1,20,7,9,20,10,11,7 };
+//	int j = 0;
+//	int i = 0;
+//	for (i = 0; i < 11;++i){
+//		int count=0;
+//		for ( j=0; j < 11; ++j){
+//			if (i == j){
+//				continue;
+//			}
+//			if ((arr[i] ^ arr[j])==0){
+//				break;
+//			}
+//			else{
+//				count++;
+//			}
+//			if (count == 10){
+//				printf("单独出现的是：%d\n", arr[i]);
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+////一组数据中只有一个数字出现了一次。其他所有数字都是成对出现的。 
+////请找出这个数字。
+//int main(){
+//	int arr[11] = { 11,10,1,1,20,7,9,20,10,11,7 };
+//	int j = 0;
+//	int i = 0;
+//	for (i = 0; i < 11;++i){
+//		int count=0;
+//		for ( j=0; j < 11; ++j){
+//			if (i == j){
+//				continue;
+//			}
+//			if (arr[i] == arr[j]){
+//				break;
+//			}
+//			else{
+//				count++;
+//			}
+//			if (count == 10){
+//				printf("单独出现的是：%d\n", arr[i]);
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
 
 //#define _CRT_SECURE_NO_WARNINGS
 //#include <stdio.h>
@@ -20,8 +129,13 @@ int main(){
 //	int a, b;
 //	printf("请输入两个数字：\n");
 //	scanf("%d %d", &a, &b);
-//	double aver = (a + b) >> 1;
-//	printf("平均值为：%.4lf",aver);
+//	//求平均值，即将a与b不同的部分除以二，加到较小数的部分
+//	if (a < b){
+//		printf("平均值为：%d\n", a + ((b - a) >> 1));
+//	}
+//	else{
+//		printf("平均值为：%d\n", b + ((a - b) >> 1));
+//	}
 //	system("pause");
 //	return 0;
 //}
